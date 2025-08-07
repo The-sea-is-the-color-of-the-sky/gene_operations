a6dc43d8a83bce44db7b78b661c7e06f4609077b
Song-QH <522723song@gmail.com>
2025-08-07 22:56:39 +0800

    Delete 1.0.1.4 directory

--- 1.0.1.4/build_exe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM 需要先安装 pyinstaller: pip install pyinstaller
-REM version.txt 必须与本脚本在同一目录
-pyinstaller --noconfirm --onefile --windowed --icon=package\icon.ico --distpath . --version-file package\\version.txt main.py
-echo.
-echo 打包完成，exe在当前目录下
-pause
